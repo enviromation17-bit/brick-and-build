72a9f4afecb8e13110bb3c2636ad4ed89d482a69
enviromation17-bit <enviromation17@gmail.com>
2026-09-24 18:23:25 +0500

    Logo: exact client photo as-is with BRICKS & BUILT DEVELOPERS

---
+++ src/assets/logoData.js
@@ -0,0 +1,6 @@
+import p0 from "./logo_part_0.js";
+import p1 from "./logo_part_1.js";
+import p2 from "./logo_part_2.js";
+import p3 from "./logo_part_3.js";
+const LOGO_SRC = "data:image/jpeg;base64," + p0 + p1 + p2 + p3;
+export default LOGO_SRC;
