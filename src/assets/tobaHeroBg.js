7f299537898c13ff5642253ac88c62b4ac0e7d1d
enviromation17-bit <enviromation17@gmail.com>
2026-09-24 20:58:41 +0500

    Toba hero: add background image modules (parts 0-1)

---
+++ src/assets/tobaHeroBg.js
@@ -0,0 +1,8 @@
+import p0 from "./toba_hero_part_0.js";
+import p1 from "./toba_hero_part_1.js";
+import p2 from "./toba_hero_part_2.js";
+import p3 from "./toba_hero_part_3.js";
+import p4 from "./toba_hero_part_4.js";
+import p5 from "./toba_hero_part_5.js";
+const TOBA_HERO_BG = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4 + p5;
+export default TOBA_HERO_BG;
